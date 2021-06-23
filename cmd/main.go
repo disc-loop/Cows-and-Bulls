8dfd2a34c541202babc981e9a919ff72a2b3d792
Tom Jones <tjones@assemblypayments.com>
2021-06-23 10:47:16 +1000

    Basic game loop

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/Tom/CowsAndBulls/pkg/generate"
+	"github.com/Tom/CowsAndBulls/pkg/guess"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	secret := generate.Secret(2)
 
-	fmt.Println("Cows and Bulls")
-	fmt.Println("Enter your guess:")
+	fmt.Println("[== Cows and Bulls ==]")
+
 	for {
-		text, _ := reader.ReadString('\n')
-		fmt.Println(text)
+		fmt.Print("Enter your guess: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.ReplaceAll(input, "\n", "")
+		result := guess.Compare(secret, input)
+		fmt.Println(result)
 	}
 }
