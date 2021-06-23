bd525e34f688795cd1ea21310ee93b11cccd6cf2
Tom Jones <tjones@assemblypayments.com>
2021-06-23 11:21:18 +1000

    Construct response string

--- pkg/response/creator.go
+++ pkg/response/creator.go
@@ -1,5 +1,15 @@
 package response
 
-import "github.com/Tom/CowsAndBulls/pkg/guess"
+import (
+	"fmt"
 
-func Builder(guess guess.Accuracy)
+	"github.com/Tom/CowsAndBulls/pkg/guess"
+)
+
+func ToString(accuracy guess.Accuracy) string {
+	if accuracy == (guess.Accuracy{}) {
+		return "No matches!"
+	}
+
+	return fmt.Sprintf("Bulls: %v, Cows: %v", accuracy.Exact, accuracy.Near)
+}
