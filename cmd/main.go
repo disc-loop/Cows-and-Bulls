52c9fb5336921934c54e01cc33a30b2c91080643
Tom Jones <tjones@assemblypayments.com>
2021-06-23 10:37:11 +1000

    Get input

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Cows and Bulls")
+	fmt.Println("Enter your guess:")
+	for {
+		text, _ := reader.ReadString('\n')
+		fmt.Println(text)
+	}
 }
