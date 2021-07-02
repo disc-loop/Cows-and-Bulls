b183c5620f7d3bb4e6f2b1db7d3f3f2f38f65bf4
Tom Jones <tjones@assemblypayments.com>
2021-07-02 16:24:00 +1000

    Fix broken tests

--- pkg/guess/handler_test.go
+++ pkg/guess/handler_test.go
@@ -7,42 +7,42 @@ import (
 )
 
 func TestCompareReturnsAllExact(t *testing.T) {
-	result := Compare("1111", "1111")
+	result, _ := Compare("1111", "1111")
 
 	assert.Equal(t, Accuracy{Exact: 4}, result)
 }
 
 func TestCompareReturnsNearsCorrectly(t *testing.T) {
-	result := Compare("1234", "4321")
-	result2 := Compare("1134", "0001")
+	result, _ := Compare("1234", "4321")
+	result2, _ := Compare("1134", "0001")
 
 	assert.Equal(t, Accuracy{Near: 4}, result)
 	assert.Equal(t, Accuracy{Near: 1}, result2)
 }
 
 func TestCompareReturnsExpectedAccuracy(t *testing.T) {
-	result1 := Compare("1234", "1822")
-	result2 := Compare("1224", "1812")
+	result1, _ := Compare("1234", "1822")
+	result2, _ := Compare("1224", "1812")
 
 	assert.Equal(t, Accuracy{Exact: 1, Near: 1}, result1)
 	assert.Equal(t, Accuracy{Exact: 1, Near: 1}, result2)
 }
 
 func TestCompareReturnsEmptyOnNoMatches(t *testing.T) {
-	result := Compare("1111", "9999")
+	result, _ := Compare("1111", "9999")
 
 	assert.Equal(t, Accuracy{}, result)
 }
 func TestCompareDoesNotDuplicateMatches(t *testing.T) {
-	result1 := Compare("1134", "1011")
-	result2 := Compare("1134", "0011")
+	result1, _ := Compare("1134", "1011")
+	result2, _ := Compare("1134", "0011")
 
 	assert.Equal(t, Accuracy{Exact: 1, Near: 1}, result1)
 	assert.Equal(t, Accuracy{Exact: 0, Near: 2}, result2)
 }
 
 func TestCompareHandlesLongerNumbers(t *testing.T) {
-	result := Compare("123456789", "987654321")
+	result, _ := Compare("123456789", "987654321")
 
 	assert.Equal(t, Accuracy{Exact: 1, Near: 8}, result)
 }
